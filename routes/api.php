c11100eb1acf39f45cba9baabf937703a7f62ee9
Matheus Filipe <theuslipe26@gmail.com>
2023-06-21 23:10:50 -0300

    Feat: Realizado conexão com bd e criado model de users

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Exemplo de teste
+Route::get('agenda',function(){
+    return 'I am alive';
+});
